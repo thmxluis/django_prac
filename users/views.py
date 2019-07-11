fa433ddf277ba71c93329fa44ca2afcec177c763
Luis Gonzalez <luis.gonzalez@orbitec.pe>
2019-07-11 15:57:40 -0500

    % practica 11072019

---
+++ users/views.py
@@ -0,0 +1,10 @@
+''' Users Viws '''
+
+# Django
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render
+
+
+def login_view(request):
+    ''' Login view '''
+    return render(request, 'users/login.html')
